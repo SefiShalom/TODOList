3c03ad85508e26006545def2ec7f00535c885f12
Sefi <Sefishalom@gmail.com>
2018-03-08 23:58:51 +0200

    Version 1.0

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,6 +1,18 @@
-$('li').click(function () {
-    $(this).css({
-        color:"grey",
-        textDecoration: "line-through"
+$('ul').on("click","li",function () {
+    $(this).toggleClass("done");
+});
+
+$('ul').on("click","span",function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $('this').remove();
     });
+    event.stopPropagation();
+});
+
+$('input[type=text]').keypress(function (event) {
+    if(event.which === 13) {
+        var todoText = $(this).val();
+        $('ul').append("<li><span><i class=\"fas fa-trash-alt\"></i></span>"+" "+todoText+"</li>");
+        $(this).val("");
+    }
 });
